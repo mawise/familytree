9a30c920e0817c7e10dcdc967dcbd28ab7340be2
Matthew Wise <matthew.rs.wise@gmail.com>
2020-08-15 15:49:38 -0500

    Searching for people and adding parents

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,13 +3,21 @@ class PeopleController < ApplicationController
     if params[:name]
       @people = Person.where('name LIKE ?', "%#{params[:name]}%")
     else
-      #@people = Person.all
       @people = []
     end
   end
 
   def show
     @person = Person.find(params[:id])
+  end
+
+  def add_parent
+    @person = Person.find(params[:id])
+    if params[:name]
+      @people = Person.where('name LIKE ?', "%#{params[:name]}%")
+    else
+      @people = []
+    end
   end
 
   def edit
@@ -38,10 +46,36 @@ class PeopleController < ApplicationController
   def destroy
   end
 
+  def create_relationship
+    @child = Person.find(relationship_params[:child_id])
+    @parent = Person.find(relationship_params[:parent_id])
+    @return_person = Person.find(relationship_params[:return_id])
+    @child.parents << @parent
+    redirect_to @return_person
+  end
+
+  def remove_child
+    @child = Person.find(relationship_params[:child_id])
+    @parent = Person.find(relationship_params[:parent_id])
+    @parent.children.delete(@child)
+    redirect_to @parent
+  end
+
+  def remove_parent
+    @child = Person.find(relationship_params[:child_id])
+    @parent = Person.find(relationship_params[:parent_id])
+    @child.parents.delete(@parent)
+    redirect_to @child
+  end
+
   private
 
   def person_params
     params.require(:person).permit(:name, :birth, :notes)
+  end
+
+  def relationship_params
+    params.permit(:child_id, :parent_id, :return_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :people
+  get 'people/:id/addparent', to: 'people#add_parent', as: 'add_parent'
+  post 'relationships', to: 'people#create_relationship', as: 'create_relationship'
+  delete 'people/:parent_id/remove_child/:child_id', to: 'people#remove_child', as: 'remove_child'
+  delete 'people/:child_id/remove_parent/:parent_id', to: 'people#remove_parent', as: 'remove_parent'
+
   root to: 'people#index'
 end
