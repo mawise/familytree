9ad5f8e1de8279185faddbd26ff66213c722cc59
Matthew Wise <matthew.rs.wise@gmail.com>
2020-09-07 10:00:02 -0500

    add partners and siblings in show person view

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,4 +5,27 @@ class Person < ApplicationRecord
   has_many :parents, :through => :child_links
 
   has_many_attached :images
+
+  # people this person has had children with
+  def partners
+    p = Set.new
+    children.each do |child|
+      child.parents.each do |child_parent|
+        p.add(child_parent) unless child_parent == self
+      end
+    end
+    p
+  end
+
+  # people this person shares at least one parent with
+  def siblings
+    sibs = Set.new
+    parents.each do |parent|
+      parent.children.each do |parent_child|
+        sibs.add(parent_child) unless parent_child == self
+      end
+    end
+    sibs
+  end
+
 end
